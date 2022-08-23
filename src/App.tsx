f31b05ec525a164e79441e28cb88904085b606fc
Tayyab Tariq <tayyabtariqq100@gmail.com>
2022-08-23 16:49:11 +0500

    fix: forget password

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import react from 'react-dom'
+import ForgetPassword from './components/molecules/forget/forgetPassword';
 import Login from './components/molecules/login/login';
 
 function App() {
-  return (
+  return (<>
+    
     <Login/>
+    {/* <link to='/login'>link</link> */}
+    </>
   )
   }
 export default App;
