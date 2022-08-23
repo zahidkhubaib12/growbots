516e1983e3aacc6fc078fd52c2a5b13d08336bcd
zahidkhubaib12 <108808612+zahidkhubaib12@users.noreply.github.com>
2022-08-23 06:57:47 -0700

    Revert "Revert "Bootstrap""

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import react from 'react-dom'
+import ForgetPassword from './components/molecules/forget/forgetPassword';
 import Login from './components/molecules/login/login';
 
 function App() {
-  return (
+  return (<>
+    
     <Login/>
+    </>
   )
   }
 export default App;
